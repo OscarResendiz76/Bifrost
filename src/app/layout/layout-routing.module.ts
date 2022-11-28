f3e34ca83354ab8a3f6bed574829d10efd701bef
DuckyDucky2000 <108678639+DuckyDucky2000@users.noreply.github.com>
2022-11-28 12:34:41 -0600

    Add files via upload

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { LayoutComponent } from './layout.component';
-import { Screen1Component } from './screen1/screen1.component';
-import { Screen2Component } from './screen2/screen2.component';
 
 const routes: Routes = [
     {
@@ -20,14 +17,18 @@ const routes: Routes = [
                 loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
             },
             {
-                path: 'screen1',
-                loadChildren: () => import('./screen1/screen1.module').then(m => m.Screen1Module)
+                path: 'servers',
+                loadChildren: () => import('./servers/servers.module').then(m => m.ServersModule)
             },
             {
-                path: 'screen2',
-                component: Screen2Component
-            }
-        ]
+                path: 'licences',
+                loadChildren: () => import('./licences/licences.module').then(m => m.LicencesModule)
+            },
+            {
+                path: 'test',
+                loadChildren: () => import('./test/test.module').then(m => m.TestModule)
+            },
+        ],
     }
 ];
 
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -7,18 +7,23 @@ import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { TranslateModule } from '@ngx-translate/core';
+// import { TranslateModule } from '@ngx-translate/core';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TopnavComponent } from './components/topnav/topnav.component';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { NavComponent } from './nav/nav.component';
-import { Screen2Component } from './screen2/screen2.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LicencesModule } from './licences/licences.module';
+import { ServersModule } from './servers/servers.module';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
     imports: [
         CommonModule,
         LayoutRoutingModule,
+        LicencesModule,
+        ServersModule,
         MatToolbarModule,
         MatButtonModule,
         MatSidenavModule,
@@ -26,9 +31,10 @@ import { Screen2Component } from './screen2/screen2.component';
         MatInputModule,
         MatMenuModule,
         MatListModule,
-        TranslateModule
+        // TranslateModule,
+        HttpClientModule
     ],
-    declarations: [Screen2Component, LayoutComponent, NavComponent, TopnavComponent, SidebarComponent, ]
+    declarations: [LayoutComponent, NavComponent, TopnavComponent, SidebarComponent, ProfileComponent ]
 
 })
 export class LayoutModule { }
